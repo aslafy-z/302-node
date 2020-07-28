917d9d8e80fba497de7ad84c3ea4510dee4a7b8f
Zadkiel <hello@zadkiel.fr>
2020-07-28 18:36:31 +0200

    rebrand

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-# 301
+# serve-redirect
 A super simple HTTP redirection server
-
-## Install
-
-`npm install -g 301`
 
 ## Usage 
 
-`user@host:$ 301 --port 8080 --domain myshiz.biz `
+`$ npx serve-redirect --port 8080 --target https://www.google.com`
 
 ### Optional Arguments
 
 - port   - port service will listen on (default 8080)
 - ip     - IP address service will listen on (default 0.0.0.0)
-- domain - domain requests will redirect to (default 127.0.0.1)
-- proto  - protocol HTTP or HTTPS (default HTTP)
+- target - url requests will redirect to (default https://www.google.com)
 - code   - http response code to use (default 301)
-- health - optional port on which to listen for a health check (default is disabled). Handy for load balancers that will only accept a "200" response to keep 301 instance(s) in load.
--- index.js
+++ index.js
@@ -4,29 +4,16 @@ module.exports = function (opts) {
   var opts = opts || {};
   var ip     = opts.ip     || '0.0.0.0',
       port   = opts.port   || 8080,
-      code   = opts.code   || 301,
-      health = opts.health || undefined,
-      proto  = opts.proto  || 'http',
-      domain = opts.domain || '127.0.0.1';
+      code   = opts.code   || 302,
+      target = opts.target || 'https://www.google.com';
 
-  target = proto + '://' + domain;
- 
   console.log('Starting', code, 'redirect server...');
   http.createServer(handler).listen(port, ip);
   console.log('  Redirecting to:', target);
   console.log('  Listening on:  ', ip + ':' + port);
 
-  if (health) {
-    http.createServer(healthCheck).listen(health, ip);
-    console.log('  HealthCheck on:', ip + ':' + health);
-  }
-  
   function handler (req, res) {
-    var header = target + req.url
-    console.log('redirecting to:', header);
-    res.writeHead(code, { Location: header });
+    res.writeHead(code, { Location: target });
     res.end();
   }
-
-  function healthCheck (req, res) { res.end(); }
 }
